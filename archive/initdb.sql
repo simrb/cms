9851c9364f4de701f514f47c71bbabb36c0b0175
linyu <151210739@qq.com>
2017-09-22 05:58:28 -0700

    update readme format

--- archive/initdb.sql
+++ archive/initdb.sql
@@ -1,23 +1,19 @@
--- phpMyAdmin SQL Dump
--- version 3.3.7
--- http://www.phpmyadmin.net
+-- 
+-- 1, change the cms_db, cms_user, cms_pawd, as you want
+-- 2, and execute the file with root user
 --
--- Host: localhost
--- Generation Time: Jul 24, 2017 at 07:21 AM
--- Server version: 5.0.90
--- PHP Version: 5.2.14
+--
+
+create database cms_db;
+
+create user 'cms_user'@'localhost' identified by 'cms_pawd';
+grant all privileges on cms_db.* to cms_user@localhost identified by 'cms_pawd';
+flush privileges;
+
+use cms_db;
+
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `cms`
---
 
 -- --------------------------------------------------------
 
--- readme.md
+++ readme.md
@@ -11,16 +11,20 @@ Setup
 install the mysql, php, apache at first, and notice, 
 
 1 install the mbstring module
+
 	# yum -y install php-mbstring
 
 2 open the short tag
+
 	# echo 'short_open_tag=On' > /etc/php.d/custom.ini
 
 3 copy config file to root dir
+
 	# cp archive/cfg_template cfg.php
 	# cp archive/access_template access.php
 
 4 initialing database
+
 	# mysql -h localhost -u root < /var/www/html/project/archive/initdb.sql
 
 
