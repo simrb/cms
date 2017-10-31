8b0865e6922b457a84b4a141dc9a291a493b70f6
zcdny <151210739@qq.com>
2017-10-31 00:16:18 -0700

    adding procedure

--- archive/01upToSns.sql
+++ archive/01upToSns.sql
@@ -1,11 +1,9 @@
 -- 
--- add table record_log
---
+-- upgrade the base version to sns
 --
 
 use cms_db;
 
--- --------------------------------------------------------
 --
 -- Table structure for table `record_log`
 --
@@ -20,5 +18,17 @@ CREATE TABLE IF NOT EXISTS `record_log` (
   PRIMARY KEY  (`rlid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-
-
+--
+-- add field `sign` to table `category`
+--
+DROP PROCEDURE IF EXISTS schema_change;
+DELIMITER //
+CREATE PROCEDURE schema_change() BEGIN 
+DECLARE curr_dbs VARCHAR(100);
+SELECT DATABASE() INTO curr_dbs;
+IF NOT EXISTS (SELECT * FROM information_schema.statistics WHERE table_schema = curr_dbs AND table_name = 'category' AND index_name = 'sign') THEN
+   ALTER TABLE `category` ADD `sign` int(10) NOT NULL default '0';
+END IF;
+END//
+DELIMITER ;
+CALL schema_change();
